871c19258991936b4d805994693e515178e6f4fb
Dilnoza Bobokalonova <dilnozabobokalonova1@gmail.com>
2023-12-17 13:13:04 -0500

    fixed bug and reconstructed + printf

---
+++ 22_SPI/Inc/adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ *  Created on: Aug 16, 2023
+ *      Author: dilnozabobokalonova
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+void pa_5_adc_init(void);
+void start_conversion(void);
+uint32_t adc_read(void);
+
+#endif /* ADC_H_ */
---
+++ 22_SPI/Inc/spi.h
@@ -0,0 +1,7 @@
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "stm32f3xx.h"
+
+#endif /* SPI_H_ */
