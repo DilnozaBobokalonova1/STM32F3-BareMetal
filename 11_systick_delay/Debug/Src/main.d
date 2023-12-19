3c4cbbe172f19e1a3eb958de1957b3c7705c5f66
Dilnoza Bobokalonova <dilnozabobokalonova1@gmail.com>
2023-12-19 15:12:41 -0500

    SysTick timer delay bareMetal implementation

---
+++ 11_systick_delay/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/core_cm4.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_version.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ ../Inc/systick.h
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/core_cm4.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_version.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Include/mpu_armv7.h:
+/Users/dilnozabobokalonova/Desktop/embedded/STM32F3-BareMetal_code/chip_headers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+../Inc/systick.h:
---
+++ 11_systick_delay/Inc/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: Dec 19, 2023
+ *      Author: dilnozabobokalonova
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+
+void systick_timer_delay(int n);
+
+#endif /* SYSTICK_H_ */
